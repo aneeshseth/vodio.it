6fcf8b4d3f87a09d081e3ae644312073740a7655
Aneesh Seth <122401851+aneeshseth@users.noreply.github.com>
2024-06-02 03:14:13 -0700

    updated init to display demo

--- apps/frontend/src/app/init/page.tsx
+++ apps/frontend/src/app/init/page.tsx
@@ -1,118 +1,16 @@
-"use client";
-import * as React from "react";
-import { Button } from "@/components/ui/button";
-import { v4 as uuidv4 } from "uuid";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import AWS from "aws-sdk";
-import { useRouter } from "next/navigation";
+import React from "react";
 
-function Page() {
-  function generateRandomString(length: number) {
-    var result = "";
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-
-  const [email, setEmail] = useState<string>(generateRandomString(8));
-  const [file, setFile] = useState<File | undefined>();
-  const [loading, setLoading] = useState<boolean>(false); // State for loading indicator
-
-  AWS.config.update({
-    region: "us-east-1",
-    accessKeyId: process.env.NEXT_PUBLIC_ACCESS_ID!,
-    secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY!,
-  });
-
-  const s3 = new AWS.S3({
-    apiVersion: "2006-03-01",
-    params: { Bucket: "vodio" },
-  });
-  const router = useRouter();
-
-  async function uploadFile(keyToSend: string, type: string) {
-    setLoading(true); // Set loading to true when starting upload
-    const params = {
-      Key: `${keyToSend}.mp4`,
-      Body: file,
-      Bucket: "vodio",
-    };
-    await s3.upload(params, (err: any, data: any) => {
-      setLoading(false); // Set loading to false when upload completes
-      if (err) {
-        console.error("Error uploading file:", err);
-      } else {
-        console.log("File uploaded successfully:", data.Location);
-        if (type == "transcode") {
-          router.push(`/transcode?key=${keyToSend}&id=${email}`);
-        } else if (type == "subtitle") {
-          router.push(`/subtitles?key=${keyToSend}&id=${email}`);
-        }
-      }
-    });
-  }
-
-  async function startTranscoding() {
-    if (email === "" || !/^[a-zA-Z]+$/.test(email)) {
-      alert("please enter only letters with no spaces.");
-      return;
-    }
-    const keyToSend = uuidv4();
-    await uploadFile(keyToSend.toString(), "transcode");
-  }
-
-  async function startSubtitles() {
-    if (email === "" || !/^[a-zA-Z]+$/.test(email)) {
-      alert("please enter only letters with no spaces.");
-      return;
-    }
-    const keyToSend = uuidv4();
-    await uploadFile(keyToSend.toString(), "subtitle");
-  }
-
+function page() {
   return (
-    <div className="">
-      <div className="h-screen w-full mt-10 md:ml-5 mr-5 flex justify-center items-center">
-        <Card className="border-2 rounded-xl bg-black m-3 -mt-10">
-          <CardHeader>
-            <CardTitle className="mb-2">start generation:</CardTitle>
-            <CardDescription className="">
-              select a video from below to transcode.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="flex">
-              <Input className="max-w-80 mb-5 mr-5" value={email} />
-              <Input
-                type="file"
-                className="mb-5"
-                onChange={(e) => setFile(e.target.files?.[0])}
-              />
-            </div>
-            <Button
-              variant={"outline"}
-              className="mr-5"
-              onClick={startTranscoding}
-              disabled={loading}
-            >
-              {loading ? "Uploading..." : "Transcode"}
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
+    <div className="flex justify-center h-screen items-center">
+      <video
+        src="https://res.cloudinary.com/dhxeo4rvc/video/upload/v1717323084/0602_z8vsyt.mov"
+        autoPlay
+        controls
+        className="w-4/6 rounded-xl"
+      />
     </div>
   );
 }
 
-export default Page;
+export default page;
