0b84d1c0aeae936303413cb34596b32532608f2a
Aneesh Seth <122401851+aneeshseth@users.noreply.github.com>
2024-06-02 03:14:51 -0700

    fix/updated button text change

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
                   router.push("/init");
                 }}
               >
-                start
+                demo
               </Button>
             </div>
           </div>
